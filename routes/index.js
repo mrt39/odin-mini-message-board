ca3daf24dc31071fb4b2594905b847aa6d852964
mrt39 <mertoktar39@gmail.com>
2023-05-01 21:13:08 +0300

    Add post-append functionality from a form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { messagesArray: messages });
 });
+
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'Express' });
+});
+
+router.post("/new", function(req,res, next){
+  var message = req.body.messageInput
+  var user = req.body.userInput
+  const post = {
+    text:message,
+    user:user,
+    added: new Date()
+  }
+  messages.push(post)
+  
+  res.redirect("/")
+})
 
 module.exports = router;
